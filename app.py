83a5b369a74b58f1a860f11ec36d0982013cfd13
M. Rifqi Akram <rifqiakram28@gmail.com>
2024-03-26 20:52:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 import streamlit as st
-import pandas as pd
-import pickle
 import joblib
+from nltk.tokenize import word_tokenize
 
 # Load the model
-with open('best_model.pkl', 'rb') as model_file:
-    model = joblib.load(model_file)
-
-# Load the test data
-X_test = joblib.load('Test Kompas\data\X_test.pkl')
-y_test = joblib.load('Test Kompas\data\y_test.pkl')
+model = joblib.load('best_model.pkl')
 
 # Streamlit UI
 def main():
     st.title("Text Category Prediction")
+
+    X_test = joblib.load('Test Kompas\data\X_test.pkl')
+    X_test_clean = joblib.load('Test Kompas\data\X_test_clean.pkl')
+
     st.write("Select a title to predict its associated text category:")
 
     # Dropdown to select title
     selected_title = st.selectbox("Choose a title:", X_test['Title'])
 
     # Find the corresponding FullText for the selected title
-    selected_fulltext = X_test.loc[X_test['Title'] == selected_title, 'FullText'].iloc[0]
+    selected_fulltext = X_test.loc[X_test['Title'] == selected_title, X_test_clean['FullText']].iloc[0]
 
     if st.button("Predict"):
-        prediction = model.predict([selected_fulltext])
+
+        X_test_clean['tokens'] = selected_fulltext.apply(lambda x: word_tokenize(x))
+        X_text_test = joblib.load('Test Kompas/tfidf_vectorizer.pkl').transform(X_test_clean['tokens'].apply(lambda x: ' '.join(x)))
+
+        prediction = model.predict([X_text_test])
         st.write(f"Predicted category for '{selected_title}': {prediction[0]}")
 
 if __name__ == "__main__":
