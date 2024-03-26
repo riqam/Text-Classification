8064c606b86ba9db20ba9655b944eb6c0f166d39
M. Rifqi Akram <rifqiakram28@gmail.com>
2024-03-26 16:27:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,32 @@
 import streamlit as st
 import pandas as pd
-import nltk
-import json
+import pickle
 import joblib
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import re
-
-# Load the best model from the .pkl file
-best_model = joblib.load('best_model.pkl')
-
-nltk.download('punkt')  # Download the punkt tokenizer
-nltk.download('stopwords')  # Download the NLTK stop words
-stop_words = set(stopwords.words('indonesian'))
 
 # Load the model
 with open('best_model.pkl', 'rb') as model_file:
     model = joblib.load(model_file)
 
 # Load the test data
-X_test = pd.read_pickle('X_test.pkl')
-y_test = pd.read_pickle('y_test.pkl')
+X_test = joblib.load('Test Kompas\data\X_test.pkl')
+y_test = joblib.load('Test Kompas\data\y_test.pkl')
 
-# Function to make prediction
-def predict_title(title):
-    # Preprocess the input
-    title = [title]  # Convert to list for consistency with model input format
-    
-    # Predict
-    prediction = model.predict(title)
-    
+# Function to predict category
+def predict_category(text):
+    prediction = model.predict([text])
     return prediction[0]
 
-# Streamlit app
+# Streamlit UI
 def main():
-    st.title("Simple Text Classification App")
-    
-    # Ask user for input
-    title_input = st.text_input("Enter the title to predict:")
-    
+    st.title("Text Category Prediction")
+    st.write("Select a text to predict its category:")
+
+    # Dropdown to select text
+    selected_text = st.selectbox("Choose a text:", X_test['FullText'])
+
     if st.button("Predict"):
-        # Perform prediction
-        prediction = predict_title(title_input)
-        st.success(f"The predicted class is: {prediction}")
+        prediction = predict_category(selected_text)
+        st.write(f"Predicted category: {prediction}")
 
 if __name__ == "__main__":
     main()
