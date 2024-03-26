f582dbfda91093d812de6f094cafe855b3337a5d
M. Rifqi Akram <rifqiakram28@gmail.com>
2024-03-26 16:36:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,22 +11,20 @@ with open('best_model.pkl', 'rb') as model_file:
 X_test = joblib.load('Test Kompas\data\X_test.pkl')
 y_test = joblib.load('Test Kompas\data\y_test.pkl')
 
-# Function to predict category
-def predict_category(text):
-    prediction = model.predict([text])
-    return prediction[0]
-
 # Streamlit UI
 def main():
     st.title("Text Category Prediction")
-    st.write("Select a text to predict its category:")
+    st.write("Select a title to predict its associated text category:")
 
-    # Dropdown to select text
-    selected_text = st.selectbox("Choose a text:", X_test['FullText'])
+    # Dropdown to select title
+    selected_title = st.selectbox("Choose a title:", X_test['Title'])
+
+    # Find the corresponding FullText for the selected title
+    selected_fulltext = X_test.loc[X_test['Title'] == selected_title, 'FullText'].iloc[0]
 
     if st.button("Predict"):
-        prediction = predict_category(selected_text)
-        st.write(f"Predicted category: {prediction}")
+        prediction = model.predict([selected_fulltext])
+        st.write(f"Predicted category for '{selected_title}': {prediction[0]}")
 
 if __name__ == "__main__":
     main()
